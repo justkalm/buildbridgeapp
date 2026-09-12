02cdf2df041d071d1f860b42ffce4ddc5a52c4b1
Moiz Patrawala <moizpatrawala@Moizs-MacBook-Air.local>
2026-09-12 12:35:00 +0530

    widen CSP img-src to unblock Blob-hosted photos

--- next.config.ts
+++ next.config.ts
@@ -67,7 +67,7 @@ const securityHeaders = [
       "script-src 'self' 'unsafe-inline'",
       "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
       "font-src 'self' https://fonts.gstatic.com",
-      "img-src 'self' data: https://*.public.blob.vercel-storage.com",
+      "img-src 'self' data: https:",
       "connect-src 'self'",
       "frame-ancestors 'none'",
       "base-uri 'self'",
